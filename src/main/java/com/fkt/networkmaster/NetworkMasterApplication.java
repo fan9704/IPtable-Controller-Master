55ae1d62f16c2ac6b20e5a0465ff1aef03e4a6f7
fan9704 <cxz123499@gmail.com>
2024-06-15 13:50:15 +0800

    Init Commit

---
+++ src/main/java/com/fkt/networkmaster/NetworkMasterApplication.java
@@ -0,0 +1,18 @@
+package com.fkt.networkmaster;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMasterApplication {
+    private static final Logger log = LoggerFactory.getLogger(NetworkMasterApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run( NetworkMasterApplication.class, args);
+        log.info("Web Document Reference http://127.0.0.1:9991/swagger-ui/index.html");
+        log.info("Server is Running on http://127.0.0.1:9991");
+    }
+
+}
---
+++ src/test/java/com/fkt/networkmaster/NetworkMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkt.networkmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
